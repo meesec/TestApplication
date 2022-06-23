991216869264bb1e16d36d926fabcc609ec0a59d
christian <chmeese@googlemail.com>
2022-06-23 14:14:00 +0200

    cleanup

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -12,43 +12,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import com.example.testapplication.ui.theme.TestApplicationTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
-//        setContent {
-//            ComposeContent()
-//        }
     }
-
 
     override fun onResume() {
         super.onResume()
-//
+
         setContentView(R.layout.activity)
-
-//        val fragment = OldFragment()
-//        supportFragmentManager.commit {
-//            add(R.id.container, fragment)
-//        }
-
         findViewById<ComposeView>(R.id.activity_compose_view).setContent {
-            ComposeContent()
-        }
-    }
-}
-
-class OldFragment : Fragment(R.layout.fragment) {
-
-    override fun onResume() {
-        super.onResume()
-
-        view?.findViewById<ComposeView>(R.id.compose_view)?.setContent {
             ComposeContent()
         }
     }
